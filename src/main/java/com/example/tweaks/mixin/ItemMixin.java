7a36902d93037239e75e57d8818cd2864e26bcb4
f3775093 <f3775093@gmail.com>
2026-03-14 22:33:34 +0000

    Update ItemMixin.java

--- src/main/java/com/example/tweaks/mixin/ItemMixin.java
+++ src/main/java/com/example/tweaks/mixin/ItemMixin.java
@@ -1,9 +1,9 @@
 package com.example.tweaks.mixin;
 
 import com.example.tweaks.TweaksMod;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.UseAnim;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.consume.UseAction;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,9 +12,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Item.class)
 public class ItemMixin {
 
-    @Inject(method = "getUseDuration", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "getMaxUseTime", at = @At("HEAD"), cancellable = true)
     private void instantEat(ItemStack stack, CallbackInfoReturnable<Integer> cir) {
-        if (TweaksMod.instantEat && stack.getUseAnimation() == UseAnim.EAT) {
+        if (TweaksMod.instantEat && stack.getUseAction() == UseAction.EAT) {
             cir.setReturnValue(1);
         }
     }

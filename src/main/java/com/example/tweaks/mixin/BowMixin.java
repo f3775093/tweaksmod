f92475422783c09257d3ec85c3bac64c9dc83caf
f3775093 <f3775093@gmail.com>
2026-03-14 22:33:45 +0000

    Update BowMixin.java

--- src/main/java/com/example/tweaks/mixin/BowMixin.java
+++ src/main/java/com/example/tweaks/mixin/BowMixin.java
@@ -1,7 +1,7 @@
 package com.example.tweaks.mixin;
 
 import com.example.tweaks.TweaksMod;
-import net.minecraft.world.item.BowItem;
+import net.minecraft.item.BowItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -9,7 +9,8 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 @Mixin(BowItem.class)
 public class BowMixin {
 
-    @ModifyArg(method = "releaseUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/BowItem;getPullProgress(I)F"))
+    @ModifyArg(method = "onStoppedUsing", 
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/item/BowItem;getPullProgress(I)F"))
     private int instantBowCharge(int useTicks) {
         if (TweaksMod.instantBow) {
             return 72000;

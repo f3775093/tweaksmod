f49b8f84f9608e1b7613b3d9518b3be052f96f8b
f3775093 <f3775093@gmail.com>
2026-03-15 00:01:00 +0000

    Update ModConfig.java

--- src/main/java/com/example/tweaks/config/ModConfig.java
+++ src/main/java/com/example/tweaks/config/ModConfig.java
@@ -2,11 +2,9 @@ package com.example.tweaks.config;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-
 import net.fabricmc.loader.api.FabricLoader;
 
 public class ModConfig {
@@ -15,37 +13,26 @@ public class ModConfig {
     public static boolean instantBow = true;
 
     private static final File FILE =
-            new File(FabricLoader.getInstance().getConfigDir().toFile(), "tweaksmod.json");
+        new File(FabricLoader.getInstance().getConfigDir().toFile(), "tweaksmod.json");
 
     private static final Gson GSON =
-            new GsonBuilder().setPrettyPrinting().create();
+        new GsonBuilder().setPrettyPrinting().create();
 
     public static void load() {
-
         try {
-
             if (FILE.exists()) {
-
                 ModConfig data = GSON.fromJson(new FileReader(FILE), ModConfig.class);
-
                 instantEat = data.instantEat;
                 instantBow = data.instantBow;
-
             } else {
                 save();
             }
-
         } catch (Exception ignored) {}
-
     }
 
     public static void save() {
-
         try {
-
             GSON.toJson(new ModConfig(), new FileWriter(FILE));
-
         } catch (Exception ignored) {}
-
     }
 }
